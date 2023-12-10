4623e2353d65b49693096d5c0de0f27d5c24e02c
Renen Donkers <rene@lotsofpixels.io>
2023-12-10 10:05:12 +0000

    fix controller PHP8.1

--- Controller/Customer/Index.php
+++ Controller/Customer/Index.php
@@ -6,10 +6,13 @@ namespace Lotsofpixels\PricelistSection\Controller\Customer;
 
 use Magento\Framework\App\Action\HttpGetActionInterface;
 use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\Event\ManagerInterface as EventManager;
+use Magento\Framework\View\Result\Page;
 use Magento\Framework\View\Result\PageFactory;
-use Magento\Framework\Controller\ResultFactory;
 use Magento\Customer\Model\Session;
-use Magento\Framework\Controller\Result\RedirectFactory;
+use Magento\Framework\Controller\ResultFactory;
+
 
 /**
  *
@@ -17,58 +20,36 @@ use Magento\Framework\Controller\Result\RedirectFactory;
 class Index implements HttpGetActionInterface
 {
     /**
-     * @var PageFactory
-     */
-    private $pageFactory;
-
-    /**
-     * @var
-     */
-
-    /**
-     * @var ResultFactory
-     */
-    protected $resultRedirect;
-    /**
-     * @var Session
-     */
-    protected $customerSession;
-    /**
      * @param PageFactory $pageFactory
-     * @param RequestInterface $request
-     */
-
-
-    protected $resultRedirectFactory;
-
-    /**
-     * @param PageFactory $pageFactory
-     * @param RequestInterface $request
      * @param Session $customerSession
-     * @param ResultFactory $result
-     * @param RedirectFactory $resultRedirectFactory
+     * @param EventManager $eventManager
+     * @param RequestInterface $request
+     * @param ResultFactory $resultFactory
      */
-    public function __construct(PageFactory      $pageFactory,
-                                Session          $customerSession,
-                                ResultFactory    $result,
-                                RedirectFactory  $resultRedirectFactory
+    public function __construct(private readonly PageFactory      $pageFactory,
+                                private readonly Session          $customerSession,
+                                private readonly EventManager     $eventManager,
+                                private readonly RequestInterface $request,
+                                private readonly resultFactory    $resultFactory
+
     )
     {
-        $this->pageFactory = $pageFactory;
-        $this->resultRedirect = $result;
-        $this->customerSession = $customerSession;
-        $this->resultRedirectFactory = $resultRedirectFactory;
-
     }
 
     /**
-     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface|\Magento\Framework\View\Result\Page
+     * @return ResultInterface|Page
      */
-    public function execute()
+    public function execute(): Page|ResultInterface
     {
-if ($this->customerSession->isLoggedIn()) {
-        return $this->pageFactory->create();
-        } else
-        {return $this->resultRedirectFactory->create()->setPath('customer/account/login/', ['_current' => true]);}
+        $this->eventManager->dispatch('lotsofpixels_pricelist_page_view', ['
+        request' => $this->request,]);
+
+        if ($this->customerSession->isLoggedIn()) {
+            return $this->pageFactory->create();
+        } else {
+            $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+            $resultRedirect->setPath('customer/account');
+            return $resultRedirect;
+        }
     }
 }
